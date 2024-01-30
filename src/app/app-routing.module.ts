671d5714d54b3d3fe0aa0aacd1df6025bf5d75c8
Jesus Anaya <jesus.anaya.developer@gmail.com>
2024-01-29 22:09:17 -0600

    se areglan las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
 import { PagesRoutingModule } from './pages/pages.routing';
+import { authRoutingModule } from './auth/auth.routing';
 
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: '**', component: NopagefoundComponent }
 ];
 
@@ -18,6 +16,7 @@ const routes: Routes = [
     RouterModule.forRoot(routes),
     PagesRoutingModule,
     RouterModule.forChild(routes),
+    authRoutingModule
   ],
   exports: [RouterModule]
 })
