2548b332b8c8f8d867aca020d54e30b51e597b29
Jesus Anaya <jesus.anaya.developer@gmail.com>
2024-02-02 23:45:45 -0600

    maquetado de settings

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,29 +4,32 @@ import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from '../app-routing.module';
 
 
-
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
 import { SharedModule } from '../shared/shared.module';
 import { FormsModule } from '@angular/forms';
 import { ComponentsModule } from '../components/components.module';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
+
 
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    ProgressComponent,
     Grafica1Component,
-    PagesComponent
+    PagesComponent,
+    ProgressComponent,
+    AccountSettingsComponent,
   ],
   exports: [
     DashboardComponent,
-    ProgressComponent,
     Grafica1Component,
-    PagesComponent
+    PagesComponent,
+    ProgressComponent,
+    AccountSettingsComponent,
   ],
   imports: [
     CommonModule,
