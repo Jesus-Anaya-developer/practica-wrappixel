7fe894ba8fc36fc68ca2999dd27a41f6bbf47cf6
Jesus Anaya <jesus.anaya.developer@gmail.com>
2024-02-05 16:26:31 -0600

    se termina la logica del login

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SettingsService } from '../services/settings.service';
 
+// * Se importa la función customInitFunction para que no marque error
+declare function customInitFunction(): any;
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
@@ -12,6 +14,8 @@ export class PagesComponent implements OnInit {
 
   constructor(private settingsServices: SettingsService) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    customInitFunction();
+  }
 
 }
