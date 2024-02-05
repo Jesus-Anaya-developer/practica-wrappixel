8e94a9b23dfe28bc3d8fd3c9fba0a0d0b5b6a1fc
Jesus Anaya <jesus.anaya.developer@gmail.com>
2024-02-04 23:29:27 -0600

    logica del cambio de tema

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-account-settings',
   templateUrl: './account-settings.component.html',
   styles: ``
 })
-export class AccountSettingsComponent {
+export class AccountSettingsComponent implements OnInit {
+
+  //Se saca de la funcion changeTheme para que sea accesible desde el html y no salte al DOM
+  public themeElement = document.querySelector('#theme');
+  public links: HTMLElement[] = []; // Initialize the "links" property
+
+  ngOnInit(): void {
+    //Al iniciar el componente, se ejecuta las siguientes funciones
+    this.links = Array.from(document.querySelectorAll('.selector'));
+    this.checkCurrentTheme();
+  }
+
+  /**
+   * Changes the theme of the application.
+   *
+   * @param theme - The name of the theme to apply.
+   */
+  changeTheme(theme: string) {
+    // If the theme exists, change the href attribute to the new theme.
+    if (this.themeElement) {
+      this.themeElement.setAttribute('href', `./assets/css/colors/${theme}.css`);
+      // Save the theme in the local storage.
+      localStorage.setItem('theme', `./assets/css/colors/${theme}.css`);
+    }
+
+    this.checkCurrentTheme();
+  }
+
+  checkCurrentTheme() {
+
+
+    this.links.forEach(elem => {
+
+      //Remover la clase working
+      elem.classList.remove('working');
+
+      // Obtener el data-theme para comparar con el tema actual
+      const btnTheme = elem.getAttribute('data-theme');
+
+      // Obtener el tema actual
+      const btnThemeUrl = `./assets/css/colors/${btnTheme}.css`;
+
+      // Obtener el tema actual del localStorage
+      const currentTheme = this.themeElement?.getAttribute('href'); // Add null check
+
+      // Comparar el tema actual con el tema del boton
+      if (btnThemeUrl === currentTheme) {
+        // Si es el mismo tema, agregar la clase working
+        elem.classList.add('working');
+      }
+    });
+  }
 
 }
--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
   styles: ``
 })
-export class PagesComponent {
+export class PagesComponent implements OnInit {
+
+  //Se saca de la funcion changeTheme para que sea accesible desde el html y no salte al DOM
+  public themeElement = document.querySelector('#theme');
+
+  ngOnInit() {
+
+    const theme = localStorage.getItem('theme') || './assets/css/colors/default-dark.css';
+    // If the theme exists, change the href attribute to the new theme.
+    if (this.themeElement) {
+      this.themeElement.setAttribute('href', theme);
+    }
+  }
 
 }
