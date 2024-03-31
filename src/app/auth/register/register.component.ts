6b0bfc8d77ea9ebc66cd6ba1365a097afaab085c
Jesus Anaya <jesus.anaya.developer@gmail.com>
2024-03-30 20:35:17 -0600

    avance validacion de formulario

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+
+  public formSubmitted = false;
+
+  public registerForm = this.fb.group({
+
+    nombre: ['', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.required
+    ]],
+
+    email: ['', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.required,
+      Validators.email,
+      Validators.pattern(/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/)]],
+
+    password: ['', [
+      Validators.required,
+      Validators.minLength(5),
+      Validators.required,
+      Validators.pattern(/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{5,}$/)]],
+
+    passwordConfirm: ['', [
+      Validators.required,
+      Validators.minLength(5),
+      Validators.required,
+      Validators.pattern(/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{5,}$/)]],
+
+    terminos: [false, Validators.requiredTrue]
+  });
+
+  constructor(private fb: FormBuilder) { }
+
+  crearUsuario() {
+    this.formSubmitted = true;
+    console.log(this.registerForm.value);
+  }
+
+  campoNoValido(campo: string): boolean {
+    return this.registerForm.get(campo)?.invalid && this.formSubmitted || false;
+  }
 
 }
