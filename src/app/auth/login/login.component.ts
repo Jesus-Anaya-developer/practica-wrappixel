31f344cfcc65395d5817881b38487553002c85fb
Jesus Anaya <jesus.anaya.developer@gmail.com>
2024-04-03 17:51:44 -0600

    Se guarda token en local storage

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { FormBuilder, Validators, FormGroup, FormControlOptions } from '@angular/forms';
+import { UsuarioService } from '../../services/usuario.service';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,11 +10,40 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent {
 
-  constructor(private router: Router) { }
+  public loginForm: FormGroup = this.fb.group({
+
+    email: ['jesus@gmail.com', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.email,
+      Validators.pattern(/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/)]],
+
+    password: ['Jesus12345', [
+      Validators.required,
+      Validators.minLength(5),
+      Validators.pattern(/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{5,}$/)]],
+
+    remember: [true]
+  }, {
+
+  } as FormControlOptions);
+
+  constructor(private fb: FormBuilder, private usuarioService: UsuarioService, private router: Router) { }
 
   login() {
-    // ... reedirigir a la página de dashboard
-    this.router.navigate(['/dashboard']);
+
+    console.log(this.loginForm.value);
+
+    this.usuarioService.login(this.loginForm.value)
+      .subscribe({
+        next: resp => {
+          console.log(resp);
+          this.router.navigateByUrl('/dashboard');
+        },
+        error: err => {
+          Swal.fire('Error', err.error.msg, 'error');
+        }
+      });
   }
 
 }
--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { RegisterForm } from '../interfaces/register-form.interfaces';
 import { environment } from '../../environments/environment.development';
+import { LoginForm } from '../interfaces/login-form.interface';
+import { tap } from "rxjs/operators";
 
 const base_url = environment.base_url;
 
@@ -14,6 +16,23 @@ export class UsuarioService {
 
   crearUsuario(formData: RegisterForm) {
     console.log('Creando usuario');
-    return this.http.post(`${base_url}/usuarios`, formData);
+    return this.http.post(`${base_url}/usuarios`, formData)
+      .pipe(
+        tap((resp: any) => {
+          console.log(resp);
+          localStorage.setItem('token', resp.token);
+        })
+      );
+  }
+
+  login(formData: LoginForm) {
+    console.log('Creando usuario');
+    return this.http.post(`${base_url}/login`, formData)
+      .pipe(
+        tap((resp: any) => {
+          console.log(resp);
+          localStorage.setItem('token', resp.token);
+        })
+      );
   }
 }
